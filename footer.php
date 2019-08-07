ceac303197f16a4d5f412df46f32ff6822724145
leodaiub <leodaiub@gmail.com>
2019-08-07 17:42:52 -0300

    added a few pages

---
+++ footer.php
@@ -0,0 +1,84 @@
+<footer class="ftco-footer bg-light ftco-section">
+                <div class="container">
+                <div class="row mb-5">
+                    <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Namore Yourself</h2>
+                        <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                        </ul>
+                    </div>
+                    </div>
+                    <div class="col-md">
+                    <div class="ftco-footer-widget mb-4 ml-md-5">
+                        <h2 class="ftco-heading-2">Menu</h2>
+                        <ul class="list-unstyled">
+                        <li><a href="#" class="py-2 d-block">Loja</a></li>
+                        <li><a href="#" class="py-2 d-block">Contato</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Ajuda</h2>
+                        <div class="d-flex">
+                            <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+                            <li><a href="#" class="py-2 d-block">Info</a></li>
+                            <li><a href="#" class="py-2 d-block">Info</a></li>
+                            <li><a href="#" class="py-2 d-block">Info</a></li>
+                            <li><a href="#" class="py-2 d-block">Info</a></li>
+                            </ul>
+                            <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">Info</a></li>
+                            <li><a href="#" class="py-2 d-block">Info</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Tem Alguma Dúvida?</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                            <li><span class="icon icon-map-marker"></span><span class="text">Endereço</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">Telefone</span></a></li>
+                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">contato@namorejoias.com.br</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos direitos reservados | Esta loja foi desenvolvida com <i class="icon-heart color-danger" aria-hidden="true"></i> por <a href="https://leonardodaiub.com" target="_blank">Leonardo Daiub</a>
+                        </p>
+                    </div>
+                </div>
+                </div>
+        <?php wp_footer(); ?>       
+        </footer> 
+        <!-- loader -->
+        <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
+        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg>
+        </div>
+            <script src="js/jquery.min.js"></script>
+            <script src="js/jquery-migrate-3.0.1.min.js"></script>
+            <script src="js/popper.min.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+            <script src="js/jquery.easing.1.3.js"></script>
+            <script src="js/jquery.waypoints.min.js"></script>
+            <script src="js/jquery.stellar.min.js"></script>
+            <script src="js/owl.carousel.min.js"></script>
+            <script src="js/jquery.magnific-popup.min.js"></script>
+            <script src="js/aos.js"></script>
+            <script src="js/jquery.animateNumber.min.js"></script>
+            <script src="js/bootstrap-datepicker.js"></script>
+            <script src="js/scrollax.min.js"></script>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+            <script src="js/google-map.js"></script>
+            <script src="js/main.js"></script> 
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,181 @@
+<?php get_header(); ?>       
+        <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+            <div class="container">
+            <a class="navbar-brand" href="index.html">NAMORE YOURSELF</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="index.html" class="nav-link">Início</a></li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                    <a class="dropdown-item" href="shop.html">Loja</a>
+                    <a class="dropdown-item" href="product-single.html">Produto</a>
+                    <a class="dropdown-item" href="cart.html">Carrinho</a>
+                    <a class="dropdown-item" href="checkout.html">Checkout</a>
+                </div>
+                </li>
+                <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+                <li class="nav-item"><a href="contact.html" class="nav-link">Contato</a></li>
+                <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+
+                </ul>
+            </div>
+            </div>
+        </nav>
+        <div class="hero-wrap js-fullheight" style="background-image: url('images/image_5.jpg');">
+            <div class="overlay"></div>
+            <div class="container">
+            <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+                <!-- <h3 class="v">Modist - Time to get dress</h3>
+                <h3 class="vr">Since - 1985</h3> -->
+                <div class="col-md-11 ftco-animate text-center">
+                <h1>Namore Yourself</h1>
+                <h2><span>Bijuterias, Roupas e Acessórios</span></h2>
+                </div>
+                <div class="mouse">
+                    <a href="#" class="mouse-icon">
+                        <div class="mouse-wheel"><span class="ion-ios-arrow-down"></span></div>
+                    </a>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="goto-here"></div>
+        <section class="ftco-section ftco-product">
+                <div class="container">
+                    <div class="row justify-content-center mb-3 pb-3">
+                <div class="col-md-12 heading-section text-center ftco-animate">
+                    <h1 class="big">Em destaque</h1>
+                    <h2 class="mb-4">Destaques:</h2>
+                </div>
+                </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="product-slider owl-carousel ftco-animate">
+                                <div class="item">
+                                    <div class="product">
+                                        <a href="#" class="img-prod"><img class="img-fluid" src="images/product-1.jpg" alt="Colorlib Template">
+                                            <span class="status">30%</span>
+                                        </a>
+                                        <div class="text pt-3 px-3">
+                                            <h3><a href="#">Young Woman Wearing Dress</a></h3>
+                                            <div class="d-flex">
+                                                <div class="pricing">
+                                                    <p class="price"><span class="mr-2 price-dc">$120.00</span><span class="price-sale">$80.00</span></p>
+                                                </div>
+                                                <div class="rating">
+                                                    <p class="text-right">
+                                                        <span class="ion-ios-star-outline"></span>
+                                                        <span class="ion-ios-star-outline"></span>
+                                                        <span class="ion-ios-star-outline"></span>
+                                                        <span class="ion-ios-star-outline"></span>
+                                                        <span class="ion-ios-star-outline"></span>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <section class="ftco-section">
+                <div class="container">
+                <div class="row justify-content-center mb-3 pb-3">
+                    <div class="col-md-12 heading-section text-center ftco-animate">
+                    <h1 class="big">Blogs</h1>
+                    <h2>Postagens Recentes</h2>
+                    </div>
+                </div>
+                <div class="row d-flex">
+                    <div class="col-md-4 d-flex ftco-animate">
+                    <div class="blog-entry align-self-stretch">
+                        <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');">
+                        </a>
+                        <div class="text mt-3">
+                        <h3 class="heading mt-3"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                        <div class="meta mb-3">
+                            <div><a href="#">Dec 6, 2018</a></div>
+                            <div><a href="#">Admin</a></div>
+                            <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+        </section>
+        <section class="ftco-section bg-light ftco-services">
+                <div class="container">
+                    <div class="row justify-content-center mb-3 pb-3">
+                <div class="col-md-12 heading-section text-center ftco-animate">
+                    <h1 class="big">Serviços</h1>
+                    <h2>Queremos que você seja bem atendido!</h2>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col-md-4 text-center d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services">
+                    <div class="icon d-flex justify-content-center align-items-center mb-4">
+                            <span class="flaticon-002-recommended"></span>
+                    </div>
+                    <div class="media-body">
+                        <h3 class="heading">Politica de Reembolso</h3>
+                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                    </div>
+                    </div>      
+                </div>
+                <div class="col-md-4 text-center d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services">
+                    <div class="icon d-flex justify-content-center align-items-center mb-4">
+                            <span class="flaticon-001-box"></span>
+                    </div>
+                    <div class="media-body">
+                        <h3 class="heading">Embalagens</h3>
+                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                    </div>
+                    </div>    
+                </div>
+                <div class="col-md-4 text-center d-flex align-self-stretch ftco-animate">
+                    <div class="media block-6 services">
+                    <div class="icon d-flex justify-content-center align-items-center mb-4">
+                            <span class="flaticon-003-medal"></span>
+                    </div>
+                    <div class="media-body">
+                        <h3 class="heading">Qualidade</h3>
+                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                    </div>
+                    </div>      
+                </div>
+                </div>
+                </div>
+        </section>
+        <section class="ftco-section-parallax">
+                <div class="parallax-img d-flex align-items-center">
+                <div class="container">
+                    <div class="row d-flex justify-content-center py-5">
+                    <div class="col-md-7 text-center heading-section ftco-animate">
+                        <h1 class="big">Se inscreva</h1>
+                        <h2>Se Inscreva em Nossa Newsletter</h2>
+                        <div class="row d-flex justify-content-center mt-5">
+                        <div class="col-md-8">
+                            <form action="#" class="subscribe-form">
+                            <div class="form-group d-flex">
+                                <input type="text" class="form-control" placeholder="Digite seu melhor e-mail">
+                                <input type="submit" value="Insreva-se" class="submit px-3">
+                            </div>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+        </section>
+<?php get_footer(); ?>       
